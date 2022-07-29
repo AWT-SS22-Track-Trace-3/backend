aba2420aced66a84076b560a8c55574b35945a74
dittmanndennis <dennis1dittmann@gmail.com>
2022-07-29 18:14:26 +0200

    revert last commit

--- src/app/database/authentication.py
+++ src/app/database/authentication.py
@@ -14,40 +14,37 @@ client = pymongo.MongoClient(
 )
 users = client["track-trace"]["users"]
 
-
 class Authentication:
 
-    # <------------------------>
-    #     Authentification
-    # <------------------------>
+#<------------------------>
+#     Authentification
+#<------------------------>
 
     def is_user(username, password):
-        user = users.find_one({"username": username}, {
-                              "password": 1, "access_lvl": 1})
+        user = users.find_one( { "username": username }, { "password": 1, "access_lvl": 1 } )
         if user is not None:
             if pwd_context.verify(password, user["password"]):
-                return [True, user["access_lvl"]]
-        return [False, None]
+                return [ True, user["access_lvl"] ]
+        return [ False, None ]
 
     def get_user(username):
-        result = users.find_one({"username": username}, {"password": 0})
+        result = users.find_one( { "username": username }, { "password": 0 } )
         if result is not None:
             return result
         return None
 
 
-# <------------------------>
+#<------------------------>
 #        API-Signup
-# <------------------------>
-
+#<------------------------>
 
     def is_username(username):
-        if users.find_one({"username": username}) is not None:
-            return {"is_username": True}
-        return {"is_username": False}
+        if users.find_one( { "username": username } ) is not None:
+            return { "is_username": True }
+        return { "is_username": False }
 
     def signup(new_user):
-        if users.find_one({"username": new_user["username"]}) is not None:
+        if users.find_one( { "username": new_user["username"] } ) is not None:
             return False
 
         new_user["password"] = pwd_context.hash(new_user["password"])
--- src/app/routers/authentication.py
+++ src/app/routers/authentication.py
@@ -10,12 +10,14 @@ from ..database.authentication import Authentication
 from ..constants import JWT
 
 
+
 # Definition - access_lvl
-# 0 Wholesaler, can checkin, checkout, and view associated products
-# 1 Consumer, can checkin, terminate, and view associated products
-# 2 Manufacturer, can create, checkout, and view associated products
-# 3 Authorities, can signup users and view everything
-# 4 Admin, has admin access
+        # 0 Wholesaler, can checkin, checkout, and view associated products
+        # 1 Consumer, can checkin, terminate, and view associated products
+        # 2 Manufacturer, can create, checkout, and view associated products
+        # 3 Authorities, can signup users and view everything
+        # 4 Admin, has admin access
+
 
 
 router = APIRouter(
@@ -25,59 +27,51 @@ router = APIRouter(
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-# <------------------------>
+
+#<------------------------>
 #     Authentication
-# <------------------------>
+#<------------------------>
 
 class Token(BaseModel):
     access_token: str
-    refresh_token: str = None
     token_type: str
     access_lvl: int
-
 
 def create_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + \
-        timedelta(minutes=JWT['ACCESS_TOKEN_EXPIRE_MINUTES'])
-    to_encode.update({"exp": expire})
+    expire = datetime.utcnow() + timedelta(minutes=JWT['ACCESS_TOKEN_EXPIRE_MINUTES'])
+    to_encode.update({ "exp": expire })
 
     return jwt.encode(to_encode, JWT['SECRET_KEY'], algorithm=JWT['ALGORITHM'])
-
 
 def create_refresh_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + \
-        timedelta(minutes=JWT['REFRESH_TOKEN_EXPIRE_MINUTES'])
-    to_encode.update({"exp": expire})
+    expire = datetime.utcnow() + timedelta(minutes=JWT['REFRESH_TOKEN_EXPIRE_MINUTES'])
+    to_encode.update({ "exp": expire })
 
     return jwt.encode(to_encode, JWT['SECRET_REFRESH_KEY'], algorithm=JWT['ALGORITHM'])
-
 
 @router.post("/token", response_model=Token)
 async def token(form_data: OAuth2PasswordRequestForm = Depends()):
     user_info = Authentication.is_user(form_data.username, form_data.password)
     if not user_info[0]:
-        raise HTTPException(
-            status_code=400, detail="Incorrect username or password!")
+        raise HTTPException(status_code=400, detail="Incorrect username or password!")
     return {
-        "access_token": create_token({"sub": form_data.username}),
-        "refresh_token": create_refresh_token({"sub": form_data.username}),
+        "access_token": create_token({ "sub": form_data.username }),
+        "refresh_token": create_refresh_token({ "sub": form_data.username }),
         "token_type": "bearer",
         "access_lvl": user_info[1]
     }
 
-
 class User(BaseModel):
     username: str
-    password: str | None = None
+    password: str | None = None 
     company: str | None = None
     country: str
     address: str | None = None
     access_lvl: int
-
 
 async def authenticate(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -87,19 +81,17 @@ async def authenticate(token: str = Depends(oauth2_scheme)):
     )
 
     try:
-        payload = jwt.decode(
-            token, JWT['SECRET_KEY'], algorithms=JWT['ALGORITHM'])
+        payload = jwt.decode(token, JWT['SECRET_KEY'], algorithms=JWT['ALGORITHM'])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
-
+    
     user: User = Authentication.get_user(username)
     if user is None:
         raise credentials_exception
     return user
-
 
 async def authenticate_refresh(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -109,20 +101,18 @@ async def authenticate_refresh(token: str = Depends(oauth2_scheme)):
     )
 
     try:
-        payload = jwt.decode(
-            token, JWT['SECRET_REFRESH_KEY'], algorithms=JWT['ALGORITHM'])
+        payload = jwt.decode(token, JWT['SECRET_REFRESH_KEY'], algorithms=JWT['ALGORITHM'])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
-
+    
     user: User = Authentication.get_user(username)
     if user is None:
         raise credentials_exception
     return user
 
-
 @router.get("/refresh", response_model=Token)
 async def refresh(user: User = Depends(authenticate_refresh)):
-    return {"access_token": create_token({"sub": user.username}), "token_type": "bearer", "access_lvl": user.access_lvl}
+    return { "access_token": create_token({ "sub": user.username }), "token_type": "bearer", "access_lvl": user.access_lvl }
