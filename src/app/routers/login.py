fadf4a3edf0b540ae42904eaa24a28cde7424637
unknown <f.berndt99@gmail.com>
2022-08-20 16:09:18 +0200

    docs

--- src/app/routers/login.py
+++ src/app/routers/login.py
@@ -1,10 +1,13 @@
+from pdb import post_mortem
 from fastapi import APIRouter, HTTPException, Depends
+from fastapi.responses import JSONResponse
 from pycountry import countries
 from pydantic import BaseModel
 
 from ..database.authentication import Authentication
 from .authentication import authenticate
 from ..database.models.models import User, NewUser
+from ..database.models.http_responses import *
 
 
 
@@ -25,22 +28,33 @@ router = APIRouter(
 #        API-Login
 #<------------------------>
 
-@router.get("/is_username/{username}")
+@router.get("/is_username/{username}", responses=default_responses)
 async def is_username(username: str, user: User = Depends(authenticate)):
+    """
+    Check if given username already exists.
+    """
     if user["access_lvl"] != 3 and user["access_lvl"] != 4:
         raise HTTPException(status_code=403, detail="Insufficient authorization level!")
 
-    return Authentication.is_username(username)
+    return JSONResponse(status_code=200, content=Authentication.is_username(username))
 
-@router.post("/signup")
+post_responses = {
+    **default_responses,
+    400: {"model": ModelMessage, "description": "Invalid country or user."},
+}
+
+@router.post("/signup", responses=post_responses)
 async def signup(new_user: NewUser, user: User = Depends(authenticate)):
+    """
+    Register a new user in the system. Only accessible for access level >= 3 (authority).
+    """
     if user["access_lvl"] != 3 and user["access_lvl"] != 4:
-        raise HTTPException(status_code=403, detail="Insufficient authorization level!")
+        return JSONResponse(status_code=403, content={"message": "Insufficient authorization level!"})
 
     if countries.get(alpha_2=new_user.country) is None:
-        raise HTTPException(status_code=400, detail="Country does not exist!")
+        return JSONResponse(status_code=400, content={"message": "Country does not exist!"})
 
     if not Authentication.signup(new_user.dict()):
-        raise HTTPException(status_code=406, detail="User already exists!")
+        return JSONResponse(status_code=400, content={"message": "User already exists!"})
 
-    return { "acknowledged": True }
+    return JSONResponse(status_code=200, content={"acknowledged": True})
